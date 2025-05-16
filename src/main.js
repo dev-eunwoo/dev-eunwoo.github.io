9032cba296c748418b78b9dddc29c3e170514bca
dev-eunwoo <dev.eunwoo@gmail.com>
2025-05-16 20:04:48 +0900

    Observer-Part2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 'use strict';
 
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
@@ -18,14 +18,14 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Arrow up 버튼을 아래로 스크롤시 투명하게 처리함
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
-    if(window.scrollY > homeHeight /2) {
-        arrowUp.style.opacity = 1;
-    } else {
-      arrowUp.style.opacity = 0;
-    }
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
 });
 
 // Navbar 토글버튼 클릭 처리
